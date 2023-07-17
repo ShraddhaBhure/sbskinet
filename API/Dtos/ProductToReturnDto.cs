8dac334c355c38949951e1a8e25b960e58252316
Shraddha Bhure <122206885+ShraddhaBhure@users.noreply.github.com>
2023-07-17 16:33:16 +0530

    End of Section 11

--- API/Dtos/ProductToReturnDto.cs
+++ API/Dtos/ProductToReturnDto.cs
@@ -5,7 +5,7 @@ namespace API.Dtos
        public int Id { get; set; }
        public string Name { get; set; }
 
-       public string Description { get; set; } 
+       public string Description { get; set; }
 
        public decimal Price { get; set; }
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import{HttpClientModule} from '@angular/common/http';
+import{HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 
 @NgModule({
@@ -22,7 +23,9 @@ import { HomeModule } from './home/home.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS , useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
